3f6943c78c75f858ad6044676c0611b4b0068fc2
take <ryoma.take@toppan.co.jp>
2022-07-08 13:59:43 +0900

    PINとMemberBarcodeを実装

--- member_ship_card/member_barcode.go
+++ member_ship_card/member_barcode.go
@@ -1,1 +1,18 @@
 package membershipcard
+
+import "errors"
+
+type MemberBarcode struct {
+	value string
+}
+
+func NewMemberBarcode(value string) (*MemberBarcode, error) {
+	if len(value) == 16 {
+		return &MemberBarcode{
+			value: value,
+		}, nil
+	}
+	return nil, errors.New(
+		"MemberBarcode length must be 16",
+	)
+}
--- member_ship_card/pin.go
+++ member_ship_card/pin.go
@@ -1,1 +1,18 @@
 package membershipcard
+
+import "errors"
+
+type Pin struct {
+	value string
+}
+
+func NewPin(value string) (*Pin, error) {
+	if len(value) == 4 {
+		return &Pin{
+			value: value,
+		}, nil
+	}
+	return nil, errors.New(
+		"PIN length must be four",
+	)
+}
