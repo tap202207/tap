6db8e46ddbdeb1942cc3f9e9bd6dbcc9c831e2f6
take <ryoma.take@toppan.co.jp>
2022-07-08 14:21:57 +0900

    membershipcardとmemberidを実装

--- member_ship_card/member_id.go
+++ member_ship_card/member_id.go
@@ -1,1 +1,18 @@
 package membershipcard
+
+import "errors"
+
+type MemberId struct {
+	value string
+}
+
+func NewMemberId(value string) (*MemberId, error) {
+	if len(value) == 64 {
+		return &MemberId{
+			value: value,
+		}, nil
+	}
+	return nil, errors.New(
+		"MemberId length must be 64",
+	)
+}
---
+++ member_ship_card/membership_card.go
@@ -0,0 +1,8 @@
+package membershipcard
+
+type MembershipCard struct {
+	MemberID      *MemberId
+	CompanyCode   *CompanyCode
+	Pin           *Pin
+	MemberBarcode *MemberBarcode
+}
